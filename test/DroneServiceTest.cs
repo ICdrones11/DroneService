ecc6b1affa73b9a999c5c2a4988c6d3ae0d23e9f
Monish OUTLOOK <Monish OUTLOOK>
2016-10-20 11:32:16 +0100

    [PB] trying to get xunit test runner working with dotnet test

---
+++ test/DroneServiceTest.cs
@@ -0,0 +1,13 @@
+using xunit;
+
+namespace DroneServiceTests
+{
+    public class DroneServiceTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 4);
+        }
+    }
+}
